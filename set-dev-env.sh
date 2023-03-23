7db59eb7902bfcad72ebcacdcbfefcf1dd0b9c15
HunterretnuH <m.gzela@gmail.com>
2023-03-24 00:09:38 +0100

    Locked pureline to older version and small fixes.
    Cloned repo for pureline was temporarily checked out to older commit.
    It was done due to errors showing with tmux when using new version.
    To use new version I would also have to modify .pureline.conf to
    overwrite new functions correctly which is necessary to gave vi mode
    indicator.

--- set-dev-env.sh
+++ set-dev-env.sh
@@ -14,27 +14,30 @@
 BIN_DIR_PATH=/home/$USER/MyPrograms/bin
 PATH=$PATH:$BIN_DIR_PATH
 #}}}
+#{{{ Copy configs
+cp -fr .custom_config /home/$USER/
+#}}}
 #{{{ Bash - .bashrc
 if ! grep -q "SOURCE CUSTOM CONFIG " /home/$USER/.bashrc; then
-    echo -e "\n\n####### SOURCE CUSTOM CONFIG #######\nsource /home/$USER/.custom_config/.bashrc\n##############" >> /home/$USER/.bashrc
+    echo -e "\n####### SOURCE CUSTOM CONFIG #######\nsource /home/$USER/.custom_config/.bashrc\n##############" >> /home/$USER/.bashrc
 fi
 #}}}
 #{{{ Readline: - .inputrc
 if ! grep -q "SOURCE CUSTOM CONFIG " /home/$USER/.inputrc; then
-    echo -e "\n\n####### SOURCE CUSTOM CONFIG #######\n\$include /home/$USER/.custom_config/.inputrc\n##############" >> /home/$USER/.inputrc
+    echo -e "\n####### SOURCE CUSTOM CONFIG #######\n\$include /home/$USER/.custom_config/.inputrc\n##############" >> /home/$USER/.inputrc
 fi
 #}}}
 #{{{ File editor: VIM - .vimrc
 if ! grep -q "SOURCE CUSTOM CONFIG " /home/$USER/.vimrc; then
-    echo -e "\n\n####### SOURCE CUSTOM CONFIG #######\nsource /home/$USER/.custom_config/.vimrc\n##############" >> /home/$USER/.vimrc
+    echo -e "\n\"####### SOURCE CUSTOM CONFIG #######\nsource /home/$USER/.custom_config/.vimrc\n\"##############" >> /home/$USER/.vimrc
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     mkdir ~/Wiki
 fi
 #}}}
-#{{{ Terminal multiplexer: TMUX - tmux.conf
+#{{{ Terminal multiplexer: TMUX - .tmux.conf
 if ! grep -q "SOURCE CUSTOM CONFIG " /home/$USER/.tmux.conf; then
-    echo -e "\n\n####### SOURCE CUSTOM CONFIG #######\nsource-file /home/$USER/.custom_config/.tmux.conf\n##############" >> /home/$USER/tmux.conf
+    echo -e "\n####### SOURCE CUSTOM CONFIG #######\nsource-file /home/$USER/.custom_config/.tmux.conf\n##############" >> /home/$USER/.tmux.conf
 fi
 cp -fr .tmux /home/$USER/
 git clone https://github.com/sunaku/tmux-navigate /home/$USER/.tmux/plugins/tmux-navigate
@@ -45,8 +48,10 @@ git clone https://github.com/sunaku/tmux-navigate /home/$USER/.tmux/plugins/tmux
     ln -s /home/$USER/MyPrograms/ranger/ranger.py $BIN_DIR_PATH/ranger
 #}}}
 #{{{ Powerline: PURELINE
-git clone https://github.com/ranger/ranger 
 git clone https://github.com/chris-marsh/pureline /home/$USER/MyPrograms/pureline
+cd /home/$USER/MyPrograms/pureline
+git reset --hard 5f227e4f42f1f3183dae02e65faf214323778179
+cd ~
 #}}}
 #{{{ Fuzzy Finder: FZF
 git clone https://github.com/junegunn/fzf /home/$USER/MyPrograms/fzf
@@ -54,8 +59,5 @@ cd /home/$USER/MyPrograms/fzf
 ./install --bin
 cd /home/$USER/
 ln -s /home/$USER/MyPrograms/fzf/bin/fzf $BIN_DIR_PATH/fzf
-#}}}
-#{{{ Copy configs
-cp -fr .custom_config /home/$USER/
 #}}}
 
